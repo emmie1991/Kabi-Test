53b0a114c598ac2cb7c329596ddf499b3d9a8943
emmie1991 <x.emma.emma.x@gmail.com>
2022-10-03 12:37:19 +0200

    fixed bug on return button

--- src/main.js
+++ src/main.js
@@ -4,19 +4,22 @@ $(document).ready(function(){
     let itemNo = e.currentTarget.innerText.slice(-1)
 
     //get current card details
-    let cardDetails = $('.item-' + itemNo).clone().append('<span class="btn closeBtn"><i class="fa-solid fa-chevron-left"></i>nazaj na seznam</span>')
+    let cardDetails = $('.item-' + itemNo).clone()
+    
+    cardDetails.append('<span class="btn closeBtn"><i class="fa-solid fa-chevron-left"></i>nazaj na seznam</span>')
 
     // put current card details in the modal-more-content
     $('#modal-more-content').html(cardDetails)
 
     $('.modal, .scroll-lock').addClass('active');
   })
-  $('.closeBtn, .modal').click(function(){
-    $('#modal-more-content').empty()
-    $('.modal, .scroll-lock').removeClass('active')
-  })
-})
 
+  setInterval(function(){
+    $('.closeBtn').click(function(){
+      $('#modal-more-content').empty()
+      $('.modal, .scroll-lock').removeClass('active')})
+    }, 100);
+  })
 
 
 
